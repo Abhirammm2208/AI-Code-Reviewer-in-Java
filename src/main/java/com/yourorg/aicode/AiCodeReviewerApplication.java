e5f1dadefaa22ce58247b15651c6d785dde3c0c8
Abhirammm2208 <abhiram.chintalgat@gmail.com>
2026-01-04 19:57:07 +0530

    docs: add JavaDoc to AiCodeReviewerApplication class

--- src/main/java/com/yourorg/aicode/AiCodeReviewerApplication.java
+++ src/main/java/com/yourorg/aicode/AiCodeReviewerApplication.java
@@ -15,6 +15,9 @@ package com.yourorg.aicode;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Main entry point for the AI Code Reviewer application.
+ */
 @SpringBootApplication
 public class AiCodeReviewerApplication {
     public static void main(String[] args) {
